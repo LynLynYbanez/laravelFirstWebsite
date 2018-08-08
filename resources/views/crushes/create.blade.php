f8d57367d2f467db1b76e18b1db138331f41c660
0515-3695-cect@CEC.EDU.PH <0515-3695-cect@CEC.EDU.PH>
2018-08-08 16:40:22 +0800

    Validation:

--- resources/views/crushes/create.blade.php
+++ resources/views/crushes/create.blade.php
@@ -1,17 +1,27 @@
+@if ($errors->any())
+     <div class="alert alert->danger">
+     	<ul>
+           @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+         </ul>
+      </div>
+@endif
+
 <form action="{{ $action }}" method="POST">
   {{ csrf_field() }}
 
   <label> First name:</label>
-  <input type="text" name="first_name"/>
+  <input type="text" name="first_name" value="@if (old('first_name')) {{ old('first_name') }} @else {{ $crush->first_name }} @endif"/>
   <br/>
    <label> Last name:</label>
-  <input type="text" name="last_name"/>
+  <input type="text" name="last_name" value="@if (old('last_name')) {{ old('last_name') }} @else {{ $crush->last_name }} @endif"/>
   <br/>
    <label> FB Profile Link:</label>
-  <input type="text" name="fb_profile_link"/>
+  <input type="text" name="fb_profile_link" value="@if (old('fb_profile_link')) {{ old('fb_profile_link') }} @else {{ $crush->fb_profile_link }} @endif"/>
   <br/>
    <label> Contact number:</label>
-  <input type="text" name="contact_number"/>
+  <input type="text" name="contact_number" value="@if (old('contact_number')) {{ old('contact_number') }} @else {{ $crush->contact_number }} @endif"/>
   <br/>
   <button type="submit">{{ $submit_text }}</button>
 
